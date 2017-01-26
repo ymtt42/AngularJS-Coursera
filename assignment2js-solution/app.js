87c58655e3f15ad4c13419124930759ee9440c86
Yves Matton <yves.matton.x@gmail.com>
2017-01-26 07:52:09 +0100

    Almost working solution

---
+++ assignment2js-solution/app.js
@@ -0,0 +1,59 @@
+(function() {
+'use strict';
+
+angular.module ('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.inject=['ShoppingListCheckOffService'];
+
+function ToBuyController(ShoppingListCheckOffService) {
+   var list=this;
+   list.items=ShoppingListCheckOffService.getToBuyItems();
+   console.log(list.items);
+   console.log(list.items[0].quantity);
+   list.tickItem= function (itemIndex) {
+     ShoppingListCheckOffService.tickItem(itemIndex);
+   }
+}
+
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought=this;
+  bought.items=ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService() {
+  var service=this;
+
+  var itemToBuy=[{name: "cookies", quantity: 10 }, { name: "cookies", quantity: 10 }, { name: "cookies", quantity: 10 }];
+  //console.log(itemToBuy);
+  var itemBought=[];
+
+  service.tickItem=function(itemIndex) {
+    var item = {
+      name: itemToBuy[itemIndex].name,
+      quantity: itemToBuy[itemIndex].quantity
+    };
+    itemBought.push(item);
+    console.log(item);
+    console.log(itemBought);
+
+    itemToBuy.splice(itemIndex,1);
+  };
+
+  service.getToBuyItems=function () {
+    return itemToBuy;
+  };
+
+  service.getBoughtItems=function () {
+    return itemBought;
+  };
+
+
+
+}
+
+
+})();
