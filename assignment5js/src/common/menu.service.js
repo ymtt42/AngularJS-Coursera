b58961c4a79c1ddf1193cbf398c2d00c4e2fee0b
Yves Matton <yves.matton.x@gmail.com>
2017-02-21 17:02:47 +0100

    working version without image load

--- assignment5js/src/common/menu.service.js
+++ assignment5js/src/common/menu.service.js
@@ -42,6 +42,17 @@ function MenuService($http, ApiPath) {
     item=newItem;
   };
 
+  service.checkItem=function(short_name) {
+    return $http.get(ApiPath + '/menu_items/'+short_name+'.json')
+    .then(function (response) {
+      return response.data;
+    },function (response){
+      return "Wrong item";
+      console.log("error");
+    });
+
+  };
+
 }
 
 
--- assignment5js/src/public/registration/registration.controller.js
+++ assignment5js/src/public/registration/registration.controller.js
@@ -4,14 +4,31 @@
 angular.module('public')
 .controller('RegistrationController', RegistrationController);
 
-RegistrationController.$inject = ['MenuService'];
-function RegistrationController(MenuService) {
+RegistrationController.$inject = ['MenuService','$q'];
+function RegistrationController(MenuService,$q) {
 var $ctrl=this;
+$ctrl.message="";
+
   $ctrl.submit=function() {
     //console.log("clicked");
-    console.log($ctrl.user);
-    MenuService.setUser($ctrl.user);
-    console.log(MenuService.getUser());
+    // console.log($ctrl.user);
+    // MenuService.setUser($ctrl.user);
+    // console.log(MenuService.getUser());
+    var promise = MenuService.checkItem($ctrl.user.favoritedish);
+  promise.then(function(result) {
+    if(!(result=="Wrong item")){
+      console.log(result);
+      console.log($ctrl.user);
+      MenuService.setUser($ctrl.user);
+      $ctrl.message="Your information has been saved"
+      console.log(MenuService.getUser());
+    }
+    else {
+      $ctrl.message="No such menu number exists";
+    }
+
+
+    });
   }
 }
 
